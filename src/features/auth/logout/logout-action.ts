e922694793a21cb0fb9e311366e2ffdf8748e9e0
Mark Dionnie <dionnie_bulingit@yahoo.com>
2025-08-23 03:44:29 +0800

    =up

--- src/features/auth/logout/logout-action.ts
+++ src/features/auth/logout/logout-action.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import { redirect, RedirectType } from "next/navigation";
 import { authRepositoryServer } from "../shared/repository/auth-repository-remote-server-side";
 
 export async function logoutAction() {
   await authRepositoryServer.logout();
+  redirect(`/login`, RedirectType.push);
 }
