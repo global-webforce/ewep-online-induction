a99888f5dabcb83c4cd13d10948515a0544487fd
Mark Dionnie <dionnie_bulingit@yahoo.com>
2025-10-20 13:47:39 +0800

    =up

---
+++ environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: string;
+    SUPABASE_SECRET_KEY: string;
+  }
+}
